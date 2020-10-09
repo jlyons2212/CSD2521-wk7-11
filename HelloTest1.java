16f4dcc7106a1b399dedc317315cf936592f3d85
jlyons2212 <70172457+jlyons2212@users.noreply.github.com>
2020-10-09 14:11:18 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comment on line 8, as well as added a comment on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,13 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/09/2020 by JDL
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request
+     * A moose bit my sister once. 
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
